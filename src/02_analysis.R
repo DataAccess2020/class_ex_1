c11c0eef32723f44faea198e6803744cb664673f
Francesco <fcatalfamo99@gmail.com>
2022-02-12 11:32:45 +0100

    Plots

--- src/02_analysis.R
+++ src/02_analysis.R
@@ -1,4 +1,16 @@
 library(tidyverse)
 
+#plot histogram education level
+ggplot(df, aes(eisced)) +
+  geom_bar()
+#plot histogram climate change belief
+ggplot(df, aes(clmchng)) +
+  geom_bar()
+
+#regression model
 reg <- lm(clmchng ~ eisced, data = df )
 summary(reg)
+
+#regression plot
+ggplot(df, aes(x = eisced, y = clmchng)) +
+  geom_smooth(method = "lm")
